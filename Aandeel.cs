ea34fd9ff6bf7e65e70cbb8ba0709738b9d34261
Waldo Heudens <waldo@heudens.be>
2021-10-25 14:57:37 +0200

    Add project files.

---
+++ Aandeel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Aandeel
+    {
+        public string Symbool { get; set; }
+        public string Naam { get; set; }
+        public string Land { get; set; }
+        public List<Transactie> Transacties { get; set; }
+
+        public Aandeel()
+        {
+            Transacties = new List<Transactie>();
+        }
+
+        public override string ToString()
+        {
+            return "Naam: " + Naam + ", Symbool: " + Symbool + ", Land: " + Land;
+        }
+
+        public void NieuweTransactie(DateTime datum, int aantal, double koers, Transactie.TransactieTypen type)
+        {
+            Transacties.Add(new Transactie() { Aantal = aantal, Datum = datum, Koers = koers, Type = type });
+        }
+    }
+
+    internal class Transactie
+    {
+        public enum TransactieTypen { Aankoop, Verkoop }
+        public DateTime Datum { get; set; }
+        public int Aantal { get; set; }
+        public double Koers { get; set; }
+        public TransactieTypen Type { get; set; }
+
+        public override string ToString()
+        {
+            return "    --> " + Datum.ToString() + " " + Type.ToString() + ": " + Aantal + "aan €" + Koers;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Linq;
+
+// Maak en vul een paar aandelen
+
+var aandelen = new Aandeel[3];
+
+aandelen[0] = new Aandeel() { Land = "VS", Naam = "Microsoft", Symbool = "MSTF" };
+aandelen[1] = new Aandeel() { Land = "Nederland", Naam = "TomTom", Symbool = "TMOAF" };
+aandelen[2] = new Aandeel() { Land = "VS", Naam = "Oracle", Symbool = "JAVA" };
+
+aandelen[0].NieuweTransactie(new DateTime(2021, 10, 25), 235, 33.11, Transactie.TransactieTypen.Aankoop);
+aandelen[0].NieuweTransactie(new DateTime(2021, 10, 26), 130, 35.12, Transactie.TransactieTypen.Verkoop);
+aandelen[1].NieuweTransactie(new DateTime(2021, 10, 24), 125, 102.11, Transactie.TransactieTypen.Aankoop);
+aandelen[1].NieuweTransactie(new DateTime(2021, 10, 26), 125, 102.98, Transactie.TransactieTypen.Verkoop);
+aandelen[2].NieuweTransactie(new DateTime(2021, 10, 22), 650, 15.68, Transactie.TransactieTypen.Aankoop);
+aandelen[2].NieuweTransactie(new DateTime(2021, 10, 26), 500, 15.72, Transactie.TransactieTypen.Verkoop);
+
+
+// Gebruik een lambda-expressie om te selecteren
+var lijstVS = aandelen.Where(x => x.Land == "VS" && x.Naam.StartsWith("M"));
+
+// Doe hetzelfde met een linq-statement
+var mijnLijst = from aandeel in aandelen
+                where aandeel.Land == "VS" && aandeel.Naam.StartsWith("M")
+                select aandeel;
+
+// Toon de selectie in de console
+foreach (Aandeel aandeel in mijnLijst)
+{
+    Console.WriteLine(aandeel.ToString());
+
+    foreach (Transactie transactie in aandeel.Transacties)
+        Console.WriteLine(transactie.ToString());
+}
